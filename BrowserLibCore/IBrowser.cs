34cb67889eaf768c6b323a6b583a128002054f71
myangelkamikaze <myangelkamikaze@gmail.com>
2020-06-05 11:52:40 +0900

    Revert "remove unused code"
    This reverts commit 021f13a272cbd52d2e6de67a1c94eb1eda3c0104.

---
+++ BrowserLibCore/IBrowser.cs
@@ -0,0 +1,12 @@
+﻿namespace BrowserLibCore
+{
+	public interface IBrowser
+	{
+		void ConfigurationChanged();
+		void InitialAPIReceived();
+		void Navigate(string url);
+		void CloseBrowser();
+		void SetProxy(string v);
+		void OpenExtraBrowser();
+	}
+}
